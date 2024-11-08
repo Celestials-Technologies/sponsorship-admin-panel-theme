ef28edc02e90a1d62e21e0a648d741bfb2bcc4b0
Waqas Malik <waqasmalik10@hotmail.com>
2024-11-08 20:14:54 +0500

    sign up and login pages

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors: {
+        'bgPrimary': '#191153',
+        'primary': '#FFA51F',
+        'inputText': '#333333',
+        'btnPrimary': '#0E9E59',
+        'textPrimary': '#191153',
+      },
+    },
+  },
+  plugins: [],
+}
