5c073365ed7177a2e6acd1755b82b084b59a95c4
MunibQazi12 <munibqazi770@gmail.com>
2025-02-11 19:56:06 +0500

    Set Up the sideBar and Add the Bell icon DropDown and Add the connect button Modal and Add the Liquidity Button Modal - Fintech Dashboard

--- HTML/script.js
+++ HTML/script.js
@@ -4,15 +4,34 @@ let profileDropDown = document.getElementById("profileDropDown");
 let buttonHeader = document.getElementById("buttonHeader");
 let countrySelect = document.getElementById("countrySelect");
 let countrySelected = document.getElementById("mainImg");
+let bellDropDown = document.getElementById("bellDropDown");
+let connectModal = document.getElementById("connectModal");
+let bodyOverflowModal = document.getElementById("bodyOverflowModal");
+let bodyScreenData = document.getElementById("bodyScreenData");
 
 function sideBarControl() {
   if (sideBar.style.left === "24px") {
     sideBar.style.left = "-40%";
     sideData.style.width = "100%";
+    sideData.style.transitionDelay = "0.1s";
   } else {
     sideBar.style.left = "24px";
     sideData.style.width = "calc(100% - 285px)";
+    sideData.style.transitionDelay = "0s";
   }
+}
+
+function closeDropDowns() {
+  countrySelect.style.opacity = "0";
+  countrySelect.style.marginTop = "50px";
+  countrySelect.style.zIndex = "0";
+  profileDropDown.style.opacity = "0";
+  profileDropDown.style.marginTop = "50px";
+  profileDropDown.style.zIndex = "0";
+  bellDropDown.style.opacity = "0";
+  bellDropDown.style.marginTop = "50px";
+  bellDropDown.style.zIndex = "0";
+  buttonHeader.style.zIndex = "9999";
 }
 
 function profileButton() {
@@ -25,15 +44,28 @@ function profileButton() {
     profileDropDown.style.marginTop = "20px";
     profileDropDown.style.zIndex = "90";
     buttonHeader.style.zIndex = "0";
+    countrySelect.style.opacity = "0";
+    bellDropDown.style.opacity = "0";
   } else {
-    profileDropDown.style.opacity = "0";
-    profileDropDown.style.marginTop = "50px";
-    profileDropDown.style.zIndex = "0";
-    buttonHeader.style.zIndex = "9999";
+    closeDropDowns();
   }
-  // if (profileDropDown.style.opacity=== "1") {
-  //   countrySelect.style.opacity = "0";
-  // }
+}
+
+function bellButton() {
+  if (
+    bellDropDown.style.opacity === "0" &&
+    bellDropDown.style.marginTop === "50px" &&
+    bellDropDown.style.zIndex === "0"
+  ) {
+    bellDropDown.style.opacity = "1";
+    bellDropDown.style.marginTop = "20px";
+    bellDropDown.style.zIndex = "90";
+    buttonHeader.style.zIndex = "0";
+    countrySelect.style.opacity = "0";
+    profileDropDown.style.opacity = "0";
+  } else {
+    closeDropDowns();
+  }
 }
 
 function countrySelectBtn() {
@@ -46,18 +78,59 @@ function countrySelectBtn() {
     countrySelect.style.marginTop = "20px";
     countrySelect.style.zIndex = "90";
     buttonHeader.style.zIndex = "0";
+    profileDropDown.style.opacity = "0";
+    bellDropDown.style.opacity = "0";
   } else {
-    countrySelect.style.opacity = "0";
-    countrySelect.style.marginTop = "50px";
-    countrySelect.style.zIndex = "0";
-    buttonHeader.style.zIndex = "9999";
+    closeDropDowns();
   }
-  // if (countrySelect.style.opacity === "1") {
-  //   profileDropDown.style.opacity = "0";
-  // }
 }
+
+document.addEventListener("click", function (event) {
+  if (
+    !event.target.closest(
+      "#countrySelect , #profileDropDown , #bellDropDown"
+    ) &&
+    !event.target.closest(".iconMainBox")
+  ) {
+    closeDropDowns();
+  }
+});
 
 function changeMainImage(newSrc) {
   countrySelected.src = newSrc;
   countrySelectBtn();
+}
+
+function connectBtn() {
+  if (connectModal.style.top === "-150%") {
+    connectModal.style.top = "0%";
+    connectModal.style.opacity = "1";
+    bodyOverflowModal.style.overflow = "hidden";
+    bodyScreenData.style.opacity = "0.7";
+  } else {
+    connectModal.style.top = "-150%";
+    connectModal.style.opacity = "0";
+    bodyOverflowModal.style.overflow = "auto";
+    bodyScreenData.style.opacity = "1";
+  }
+}
+function closeBtn() {
+  connectModal.style.top = "-150%";
+  liquidityModal.style.top = "-150%"
+  bodyOverflowModal.style.overflow = "auto";
+  bodyScreenData.style.opacity = "1";
+}
+
+function liquidityBtn() {
+  if (liquidityModal.style.top === "-150%") {
+    liquidityModal.style.top = "0%";
+    liquidityModal.style.opacity = "1";
+    bodyOverflowModal.style.overflow = "hidden";
+    bodyScreenData.style.opacity = "0.7";
+  } else {
+    liquidityModal.style.top = "-150%";
+    liquidityModal.style.opacity = "0";
+    bodyOverflowModal.style.overflow = "auto";
+    bodyScreenData.style.opacity = "1";
+  }
 }
