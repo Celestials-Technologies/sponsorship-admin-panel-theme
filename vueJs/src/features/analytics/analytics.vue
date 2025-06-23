3ab72221cea61e3e58dd3f406722db4b815aa2b7
Faheem Malik <faheemmalik3000@gmail.com>
2025-06-23 12:55:24 +0500

    Add analytics section UI

---
+++ vueJs/src/features/analytics/analytics.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <TitleHeader
+      title="Analytics"
+      :isButton="true"
+      @click-button="openLiquidityModal"
+    />
+    <InfoSection />
+    <AnalyticsCharts />
+    <LiquidityModal
+      v-if="showLiquidityModal"
+      @close="showLiquidityModal = false"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { InfoSection } from "@/features/analytics/ui";
+import { AnalyticsCharts } from "@/features/analytics/ui/Charts";
+import { TitleHeader } from "@/shared/ui/TitleHeader";
+import { LiquidityModal } from "@/entities/liquidity";
+const showLiquidityModal = ref(false);
+
+const openLiquidityModal = () => {
+  showLiquidityModal.value = true;
+};
+</script>
